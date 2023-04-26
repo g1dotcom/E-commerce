792cf5ad5791eeef41fb390f76f04712bf955fa0
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-26 12:59:03 +0300

    DetailComp.jsx responsive design edildi

--- src/components/detailComp/DetailComp.jsx
+++ src/components/detailComp/DetailComp.jsx
@@ -44,15 +44,19 @@ const DetailComp = ({ productDetail }) => {
   const notify = () => toast("Ürün Sepete Eklendi!");
 
   return (
-    <div className="flex gap-10 my-10">
+    <div className="flex max-md:flex-col gap-10 my-10">
       <img
-        className="w-[700px] h-[500px] object-cover bg-black "
+        className="w-[700px] max-md:w-[500px] max-md:h-[350px]  h-[500px] object-cover bg-black "
         src={productDetail?.image}
         alt=""
       />
       <div className="bg-gray-100 p-10">
-        <div className="text-4xl font-bold">{productDetail?.title}</div>
-        <div className="my-2 text-2xl">{productDetail?.description}</div>
+        <div className="text-4xl max-md:text-2xl font-bold">
+          {productDetail?.title}
+        </div>
+        <div className="my-2 text-2xl max-md:text-xl">
+          {productDetail?.description}
+        </div>
         <div className="my-2 text-2xl flex">
           <span className="font-bold">Rating</span> :{" "}
           <span className="bg-orange-400 mx-2 text-gray-200  rounded-lg">
@@ -64,13 +68,13 @@ const DetailComp = ({ productDetail }) => {
           <span className="font-bold">Count</span> :{" "}
           {productDetail?.rating?.count}
         </div>
-        <div className="my-2 text-6xl text-orange-400">
+        <div className="my-2 text-6xl max-md:text-4xl text-orange-400">
           {productDetail?.price} <span className="text-2xl font-bold">TL</span>
         </div>
         <div className="flex items-center gap-5 my-4">
           <div
             onClick={decrement}
-            className="text-4xl cursor-pointer bg-orange-400 text-white rounded-full px-2"
+            className="text-4xl max-md:text-xl cursor-pointer bg-orange-400 text-white rounded-full px-2"
           >
             -
           </div>
@@ -81,7 +85,7 @@ const DetailComp = ({ productDetail }) => {
           />
           <div
             onClick={increment}
-            className="text-4xl cursor-pointer bg-orange-400 text-white rounded-full px-2"
+            className="text-4xl max-md:text-xl cursor-pointer bg-orange-400 text-white rounded-full px-2"
           >
             +
           </div>
